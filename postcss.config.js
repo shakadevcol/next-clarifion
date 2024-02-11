5a04844726bd40df32cd52ff168f7f2e191c2494
Sergio Aristizabal <shaka-1989@hotmail.com>
2024-02-11 10:22:48 -0500

    Tailwind and postcss config added.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        "tailwindcss/nesting": {}, // Add support for nesting
+        "postcss-import": {},
+        tailwindcss: {},
+        autoprefixer: {},
+        "postcss-pxtorem": {
+            //rootValue: 16, // Set root element font size
+            propList: ["*"],
+        },
+    },
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#fe696a",
+                grayText: "#4D5254",
+            },
+            fontFamily: {
+                body: ["Manrope"],
+            },
+            spacing: {
+                15: "3.75rem",
+            },
+        },
+        container: {
+            padding: {
+                DEFAULT: "1.25rem",
+                sm: "0",
+                lg: "0",
+                xl: "0",
+                "2xl": "0",
+            },
+        },
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "992px", //1245px
+            xl: "1245px", //1280px
+            "2xl": "1536px",
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
