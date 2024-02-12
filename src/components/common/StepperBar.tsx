a7a5bd19d9cb3532597ef8ac03158ade8fad12b5
Sergio Aristizabal <shaka-1989@hotmail.com>
2024-02-12 06:21:09 -0500

    Stepper bar added.

---
+++ src/components/common/StepperBar.tsx
@@ -0,0 +1,38 @@
+import "@/assets/css/components/common/stepper-bar.css";
+
+type Props = {
+    steps: Steps[];
+};
+
+export default function StepperBar(props: Props) {
+    return (
+        <div className="stepper">
+            <div className="stepper__items">
+                {props.steps.map((step) => {
+                    return (
+                        <div key={step.id} className="stepper__item">
+                            <div
+                                className={`stepper__circle stepper__circle--${step.state}`}
+                            >
+                                {step.state !== "incompleted" && (
+                                    <img
+                                        src="/img/common/stepper/check-mark.svg"
+                                        alt="checked"
+                                        className="stepper__icon"
+                                    />
+                                )}
+                                {step.state === "incompleted" && step.id}
+                            </div>
+                            <span className="stepper__item-number">
+                                Step {step.id}:
+                            </span>
+                            <span className="stepper__item-title">
+                                {step.title}
+                            </span>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
